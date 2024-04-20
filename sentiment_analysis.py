30d52cc72b3332d498b42d0f8d83a97c2ff3955d
Hiren Ahir <80156519+7039473270@users.noreply.github.com>
2024-04-20 23:00:18 +0530

    added css

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,5 +1,5 @@
 import re
-import nltk
+import nltk # type: ignore
 from nltk.sentiment import SentimentIntensityAnalyzer
 import pandas as pd
 
@@ -7,12 +7,14 @@ import pandas as pd
 def perform_sentiment_analysis(df):
     nltk.download('vader_lexicon')
     sia = SentimentIntensityAnalyzer()
-    df['clean_text'] = df['Tweet'].apply(lambda x: ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", x).split()))
+    df['clean_text'] = df['Tweet'].apply(lambda x: ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\\w+:\\/\\/\\S+)", " ", x).split()))
     df['compound'] = df['clean_text'].apply(lambda x: sia.polarity_scores(x)['compound'])
     df['sentiment'] = df['compound'].apply(lambda score: 'positive' if score >= 0 else 'negative')
     return df
 
 #Example usage
-df = pd.read_csv('tweets_data_world_Cup.csv')
+df = pd.read_csv('tweets_data_world_Cup.csv')#
+#df = pd.read_csv('Avengers.csv')
+#df = pd.read_csv('PalestineTweets.csv', encoding='latin1')
 df = perform_sentiment_analysis(df)
 print(df.head())
